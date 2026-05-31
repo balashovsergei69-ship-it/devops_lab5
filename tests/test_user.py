3729e3413b108498a47606f0bff4d1f668fcac43
Sergey Balashov <balashovsergei69@gmail.com>
2026-05-31 17:20:00 -0400

    Add test workflow

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,7 +4,7 @@ from src.main import app
 
 client = TestClient(app)
 
-# Существующие пользователи
+# Сущест=вующие пользователи
 users = [
     {
         'id': 1,
@@ -18,24 +18,39 @@ users = [
     }
 ]
 
+
 def test_get_existed_user():
     '''Получение существующего пользователя'''
-    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    response = client.get('/api/v1/user', params={'email': users[0]['email']})
     assert response.status_code == 200
     assert response.json() == users[0]
 
+
 def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
+    response = client.get('/api/v1/user', params={'email': 'ghost@example.com'})
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
     pass
+
 
 def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
+    payload = {'name': 'Test User', 'email': 'test@example.com'}
+    response = client.post('/api/v1/user', json=payload)
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
     pass
+
 
 def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
+    payload = {'name': 'Duplicate User', 'email': users[0]['email']}
+    response = client.post('/api/v1/user', json=payload)
+    assert response.status_code == 409
     pass
 
+
 def test_delete_user():
-    '''Удаление пользователя'''
+    payload = {'name': 'Delete User', 'email': 'delete@example.com'}
+    client.post('/api/v1/user', json=payload)
+    response = client.delete('/api/v1/user', params={'email': payload['email']})
+    assert response.status_code == 204
     pass
